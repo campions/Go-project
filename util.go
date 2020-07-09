128a95c20dd7623ca099759bef2edc80403882c2
Marian Brinza <mbrinza@mcclatchyservices.com>
2020-07-09 12:41:31 +0300

    add validate rocket coordinates

--- util.go
+++ util.go
@@ -6,3 +6,10 @@ func printPlayerBoard(player Player) {
 	fmt.Printf("Player %v board: \n", player.name)
 	fmt.Println(player.board.printBoard())
 }
+
+func validateRocketCoordinates(x int, y int) bool {
+	if x < 0 || x > 9 || y < 0 || y > 9 {
+		return false
+	}
+	return true
+}
