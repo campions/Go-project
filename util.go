3c701b541b82ea98af34a8f56e7d340f1965d44b
Marian Brinza <mbrinza@mcclatchyservices.com>
2020-07-09 14:35:42 +0300

    fnish checkIfHitOrMiss function

--- util.go
+++ util.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
-func printPlayerBoard(player Player) {
-	fmt.Printf("%v's board: \n", player.name)
-	fmt.Println(player.board.printBoard())
+func printPlayerBoard(player Player, enemyBoard bool) {
+
+	if !enemyBoard {
+		fmt.Printf("%v's board: \n", player.name)
+		fmt.Println(player.board.printBoard())
+	} else {
+		fmt.Printf("Enemy's board: \n")
+		fmt.Println(player.enemyBoard.printBoard())
+	}
 }
 
 func validateRocketCoordinates(x int, y int) bool {
@@ -14,6 +20,12 @@ func validateRocketCoordinates(x int, y int) bool {
 	return true
 }
 
-func checkIfHitOrMiss(board Board, x int, y int){
+func checkIfHitOrMiss(board Board, x int, y int) string {
 
+	// if hit
+	if board[x][y] == SHIP {
+		return "hit"
+	} else {
+		return "miss"
+	}
 }
